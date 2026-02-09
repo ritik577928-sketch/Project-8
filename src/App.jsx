ec2e3d16dc3b12f86ed5dc5fbd737389f387c47d
ritik577928-sketch <ritik577928@gmail.com>
2026-02-09 08:22:12 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,99 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const APIURL = "https://jsonplaceholder.typicode.com/users";
+
+function App() {
+  const [data, setData] = useState([]);
+  const [showpopup, setShowpopup] = useState(false);
+  const [selectedUser, setSelectedUser] = useState(null);
+
+  useEffect(() => {
+    fetch(APIURL)
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  const showUserInfo = (user) => {
+    setSelectedUser(user);
+    setShowpopup(true);
+  };
+
+  const closeUserInfo = () => {
+    setSelectedUser(null);
+    setShowpopup(false);
+  };
+
+  return (
+    <div className="app">
+      <div className="header">
+        Example for APIs, Fetch function, Conditional Rendering
+      </div>
+
+      <div className="section">
+        <h1>WELCOME</h1>
+
+        <table border="1">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Username</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {data.map((user) => (
+              <tr key={user.id} onClick={() => showUserInfo(user)}>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.username}</td>
+                <td>{user.email}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <div className="footer">
+        © CopyRight@2026 Course Catalog. All rights reserved
+      </div>
+
+      {showpopup && selectedUser && (
+        <div className="overlay">
+          <div className="popup">
+            <div className="popupHeader">
+              <button onClick={closeUserInfo}>X</button>
+            </div>
+
+            <div className="popupSection">
+              <p><b>ID:</b> {selectedUser.id}</p>
+              <p><b>Name:</b> {selectedUser.name}</p>
+              <p><b>Username:</b> {selectedUser.username}</p>
+              <p><b>Email:</b> {selectedUser.email}</p>
+
+              <p>
+                <b>Address:</b>{" "}
+                {selectedUser.address?.street},
+                {selectedUser.address?.city} -
+                {selectedUser.address?.zipcode}
+              </p>
+
+              <p><b>Phone:</b> {selectedUser.phone}</p>
+              <p><b>Website:</b> {selectedUser.website}</p>
+
+              <p>
+                <b>Company:</b> {selectedUser.company?.name}<br />
+                {selectedUser.company?.bs}
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
